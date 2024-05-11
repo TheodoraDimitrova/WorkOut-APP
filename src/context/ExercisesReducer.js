4546c55ad5d4440fd611d4aefae64a58da0ae968
teodora <tbd@shippii.com>
2024-05-12 00:43:27 +0300

    Fix ESLint error by naming exercisesReducer function

--- src/context/ExercisesReducer.js
+++ src/context/ExercisesReducer.js
@@ -1,29 +1,29 @@
-export default (state, action) => {
+const exercisesReducer = (state, action) => {
   switch (action.type) {
     case "ALL_EXERCISES":
-        return {
-          ...state,
-          exercises: action.payload,
-          exercisesResult:action.payload
-        
-        };
+      return {
+        ...state,
+        exercises: action.payload,
+        exercisesResult: action.payload,
+      };
     case "SEARCH_EXERCISES":
       return {
         ...state,
-        exercisesResult: action.payload
-      
+        exercisesResult: action.payload,
       };
     case "SET_BODY_PART":
       return {
         ...state,
-        bodyPart: action.payload
+        bodyPart: action.payload,
       };
-      case "SET_EXERCISE":
-        return {
-          ...state,
-          exercise : action.payload
-        };
+    case "SET_EXERCISE":
+      return {
+        ...state,
+        exercise: action.payload,
+      };
     default:
       return state;
   }
 };
+
+export default exercisesReducer;
